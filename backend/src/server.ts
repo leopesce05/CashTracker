98797676233ab6e5b075a9d8eba1b3114eebbeed
Leonardo Pesce <leonardo.pesce2003@gmail.com>
2025-01-28 14:18:42 -0300

    Conexion Postgres con sequelize

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,19 @@
 import express from 'express' 
 import colors from 'colors'
 import morgan from 'morgan'
+import {db} from './config/db'
+
+async function connectDB(){
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log(colors.blue.bold('Database connected'))
+    } catch (error) {
+        console.log(colors.red("Fallo la conexion"))
+    }
+} 
+
+connectDB()
 
 const app = express()
 
