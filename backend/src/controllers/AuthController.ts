c92b0ea99d7885d03b9b23e893fa75855f8ef317
Leonardo Pesce <leonardo.pesce2003@gmail.com>
2025-01-31 13:10:35 -0300

    registro usuario, falta mail de confirmacion de token

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,8 +1,26 @@
 import { Response,Request } from 'express';
+import { User } from '../models/User';
+import { hashPassword,comparePassword } from '../utils/auth';
+import { generateToken } from '../utils/tokens';
 
 
 export class AuthController {
     static async createAccount(req:Request, res:Response) {
-        console.log('createAcount')
+        try {
+            const exist = await User.findOne({ where: { email: req.body.email } });
+            if (exist) {
+                res.status(409 ).json({error: 'El email ya esta registrado'})
+                return
+            }
+            const user = new User(req.body)
+            user.password = await hashPassword(req.body.password)
+            user.token = generateToken()
+            await user.save()
+            res.status(201).json(user)
+            return
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({error: 'Error al crear usuario'})
+        }
     }
 }
--- backend/src/routes/authRouter.ts
+++ backend/src/routes/authRouter.ts
@@ -1,9 +1,22 @@
 import { AuthController } from './../controllers/AuthController';
 import {Router} from 'express';
+import {body} from 'express-validator';
+import { handleInputErrors } from '../middlewares/validation';
 
 const router = Router();
 
 
-router.post('/create-acount',AuthController.createAccount)
+router.post('/create-acount',
+    [
+        body('name')
+            .notEmpty().withMessage('El nombre es requerido'),
+        body('password')
+            .isLength({min: 8}).withMessage('La contraseña debe tener minimo 8 caracteres'),
+        body('email')
+            .isEmail().withMessage('Email no valido')
+    ],
+    handleInputErrors,
+    AuthController.createAccount
+)
 
 export default router;
