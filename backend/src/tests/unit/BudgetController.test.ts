3cd3871b655a1d06c6f85f66485064a46d9bc5fc
Leonardo Pesce <leonardo.pesce2003@gmail.com>
2025-02-04 18:21:26 -0300

    primeros tests

--- backend/src/tests/unit/BudgetController.test.ts
+++ backend/src/tests/unit/BudgetController.test.ts
@@ -1,10 +1,70 @@
-import {budgets} from '../mocks/budgets';
+import { createRequest, createResponse } from 'node-mocks-http';
+import { budgets } from '../mocks/budgets';
+import BudgetController from '../../controllers/BudgetController';
+import Budget  from '../../models/Budget';
 
-describe('BudgetController', () => {
+// 🔹 Forma correcta de mockear Budget en TypeScript
+jest.mock('../../models/Budget', () => ({
+    findAll: jest.fn(),
+}));
 
-    it('Should retrive 3 budgets',()=>{
-        expect(budgets).toHaveLength(3)
-    })
+describe('BudgetController - GetAll', () => {
+    beforeEach(() => {
 
+        (Budget.findAll as jest.Mock).mockReset();
+        (Budget.findAll as jest.Mock).mockImplementation(async (options) => {
+            const updatedBudgets = budgets.filter(
+                (budget) => budget.userId === options.where.userId
+            );
+            return Promise.resolve(updatedBudgets);
+        });
+    });
 
-})
+    it('should retrieve 2 budgets for user with ID 1', async () => {
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: { id: 1 },
+        });
+        const res = createResponse();
+
+        await BudgetController.getAll(req, res);
+
+        const data = res._getJSONData();
+        expect(data).toHaveLength(2);
+        expect(res.statusCode).toBe(200);
+        expect(res.status).not.toBe(404);
+    });
+
+    it('should retrieve 2 budgets for user with ID 1', async () => {
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: { id: 2 },
+        });
+        const res = createResponse();
+
+        await BudgetController.getAll(req, res);
+
+        const data = res._getJSONData();
+        expect(data).toHaveLength(1);
+        expect(res.statusCode).toBe(200);
+        expect(res.status).not.toBe(404);
+    });
+
+    it('should retrieve 2 budgets for user with ID 1', async () => {
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: { id: 10 },
+        });
+        const res = createResponse();
+
+        await BudgetController.getAll(req, res);
+
+        const data = res._getJSONData();
+        expect(data).toHaveLength(0);
+        expect(res.statusCode).toBe(200);
+        expect(res.status).not.toBe(404);
+    });
+});
